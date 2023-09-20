dcf01429ccb04964076ab35778d539f773bb6208
PavelMaken <pasha.point@gmail.com>
2023-09-20 11:51:30 +0300

    refactoring

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,3 +6,7 @@ const program = new Command();
 program 
  .version('0.1.0')
  .description('Compares two configuration files and shows a differense.')
+ .option('-f, --format [type]', 'format', 'stylish')
+ .argument('<filepath1> <filepath2>')
+
+program.parse(process.argv);
