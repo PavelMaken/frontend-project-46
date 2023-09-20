91d0e47a0320900b750ad28ef2e2bcf45064f47b
PavelMaken <pasha.point@gmail.com>
2023-09-20 12:28:29 +0300

    add .argumet add .action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,8 @@ program
  .description('Compares two configuration files and shows a differense.')
  .option('-f, --format [type]', 'format', 'stylish')
  .argument('<filepath1> <filepath2>')
+ .action((filePath1, filePath2) => {
+    console.log(gendiff(filePath1, filePath2, program.opts().format));
+ });
 
 program.parse(process.argv);
