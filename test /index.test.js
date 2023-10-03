b49f371d24552f3aed960eca5c486cf5f53d6b5c
PavelMaken <pasha.point@gmail.com>
2023-10-03 10:36:48 +0300

    corect

--- test /index.test.js
+++ test /index.test.js
@@ -12,6 +12,10 @@ const __dirname = dirname(fileName);
 const getFixPath = (nameFile) => path.join(__dirname, '..', '__fixtures__', nameFile);
 const readFile = (nameFile) => fs.readFileSync(getFixPath(nameFile), 'utf-8');
 
+const testDefault = readFile('OutputTest1.txt');
+const testPlain = readFile('OutputTestPlain.txt');
+const testJSON = readFile('OutputTestJson.txt');
+
 const file1 = './__fixtures__/file1.json';
 const file2 = './__fixtures__/file2.json';
 
