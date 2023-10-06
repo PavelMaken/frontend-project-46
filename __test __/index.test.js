96c20d754c028623f9e68ac7ae1c4d677d03df5f
PavelMaken <pasha.point@gmail.com>
2023-10-06 13:52:35 +0300

    refactoring code

--- __test __/index.test.js
+++ __test __/index.test.js
@@ -11,15 +11,23 @@ const __dirname = dirname(fileName);
 const getFixPath = (nameFile) => path.join(__dirname, '..', '__fixtures__', nameFile);
 const readFile = (nameFile) => fs.readFileSync(getFixPath(nameFile), 'utf-8');
 
-const testDefault = readFile('OutputTest1.txt');
+const testDefaultJSON = readFile('OutputTest1JSON.txt');
+const testDefaultYML = readFile('OutputTest1YML.txt');
 //const testPlain = readFile('OutputTestPlain.txt');
 //const testJSON = readFile('OutputTestJson.txt');
 
-const file1 = './__fixtures__/file1.json';
-const file2 = './__fixtures__/file2.json';
+const file1JSON = './__fixtures__/file1.json';
+const file2JSON = './__fixtures__/file2.json';
+
+const file1YML = './__fixtures__/file1.yml';
+const file2YML = './__fixtures__/file2.yml';
+
 
 
 test('json', () => {
-    expect(gendiff(file1 , file2)).toEqual(testDefault);
+    expect(gendiff(file1JSON , file2JSON)).toEqual(testDefaultJSON);
 });
 
+test('yml', () => {
+    expect(gendiff(file1YML , file2YML)).toEqual(testDefaultYML);
+});
